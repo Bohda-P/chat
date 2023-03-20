37fb02ce90aa73639dba9418c1664e6ea585a165
Bohdan Pasieka <bohdan_pasieka@epam.com>
2023-03-20 15:56:27 +0200

    add env

---
+++ change-env.sh
@@ -0,0 +1,11 @@
+if [ "$BITRISE_TRIGGERED_WORKFLOW_TITLE" = "qa" ]; then
+    cp ./env/.env.qa ./env/.env
+elif [ "$BITRISE_TRIGGERED_WORKFLOW_TITLE" = "dev" ]; then
+    cp ./env/.env.dev ./env/.env
+elif [ "$BITRISE_TRIGGERED_WORKFLOW_TITLE" = "staging" ]; then
+    cp ./env/.env.staging ./env/.env
+elif [ "$BITRISE_TRIGGERED_WORKFLOW_TITLE" = "prod" ]; then
+    cp ./env/.env.prod ./env/.env
+else
+    echo "No environment specified!"
+fi
