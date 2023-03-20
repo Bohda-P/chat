9eeb14b4585b3f8babcd509806d227f16bd9b7c9
Bohdan Pasieka <bohdan_pasieka@epam.com>
2023-03-20 16:14:58 +0200

    fix env

--- change-env.sh
+++ change-env.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [ "$BITRISE_TRIGGERED_WORKFLOW_TITLE" = "qa" ]; then
-    cp ./env/.env.qa ./env/.env
+    cp ./.env/.env.qa ./.env/.env
 elif [ "$BITRISE_TRIGGERED_WORKFLOW_TITLE" = "dev" ]; then
-    cp ./env/.env.dev ./env/.env
+    cp ./.env/.env.dev ./.env/.env
 elif [ "$BITRISE_TRIGGERED_WORKFLOW_TITLE" = "staging" ]; then
-    cp ./env/.env.staging ./env/.env
+    cp ./.env/.env.staging ./.env/.env
 elif [ "$BITRISE_TRIGGERED_WORKFLOW_TITLE" = "prod" ]; then
-    cp ./env/.env.prod ./env/.env
+    cp ./.env/.env.prod ./.env/.env
 else
     echo "No environment specified!"
 fi
